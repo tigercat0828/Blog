7c852c971480a343e56c5dabf529c078a916d823
Tigercat <tigercat0828@gmail.com>
2025-01-05 15:12:14 +0800

    Initial commit

---
+++ Blog.Shared/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Shared;
+
+public class Article
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+    public List<string> Tags { get; set; } = [];
+}
